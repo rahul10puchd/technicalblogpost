8dea45d4712b342e2a9aebb35afbe8ee2c950074
Rahul Kumar <rahul10.pu@gmail.com>
2021-02-11 10:37:04 +0530

    added logout feature and createPost

--- src/main/java/com/upgrad/technicalblogpost/controller/PostController.java
+++ src/main/java/com/upgrad/technicalblogpost/controller/PostController.java
@@ -5,8 +5,10 @@ import com.upgrad.technicalblogpost.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 @Controller
 public class PostController {
@@ -17,5 +19,16 @@ public class PostController {
         ArrayList<Post> posts= postService.getAllPosts();
         model.addAttribute("posts",posts);
         return "posts";
+    }
+    //TODO: GET : posts/newpost  ,  POST: post/create
+    @RequestMapping("/posts/newpost")
+    public String newPost(){
+        return "posts/create";
+    }
+    @RequestMapping(value="/posts/create", method= RequestMethod.POST)
+    public String createPost(Post newPost){
+        newPost.setDate(new Date());
+        postService.createPost(newPost);
+        return "redirect:/posts";
     }
 }
--- src/main/java/com/upgrad/technicalblogpost/service/PostService.java
+++ src/main/java/com/upgrad/technicalblogpost/service/PostService.java
@@ -8,14 +8,32 @@ import java.util.Date;
 
 @Service
 public class PostService {
-    public ArrayList<Post> getAllPosts(){
-        ArrayList<Post> posts = new ArrayList<>();
-
+    private static ArrayList<Post> POSTS=new ArrayList<Post>();
+    static {
         Post post1= new Post();
-        post1.setTitle("Boring Post");
-        post1.setBody("Boring Bodyyyyyyyyyyyyy");
+        post1.setTitle("Mirzapur");
+        post1.setBody("Kalin Bhaia ki sarkar");
         post1.setDate(new Date());
-        posts.add(post1);
-        return posts;
+
+        Post post2= new Post();
+        post2.setTitle("Gangs of Wassepur");
+        post2.setBody("Keh ke lenege");
+        post2.setDate(new Date());
+
+        Post post3= new Post();
+        post3.setTitle("Kissan");
+        post3.setBody("Modi ke sarkar");
+        post3.setDate(new Date());
+
+        POSTS.add(post1);
+        POSTS.add(post2);
+        POSTS.add(post3);
+    }
+
+    public ArrayList<Post> getAllPosts(){
+        return POSTS;
+    }
+    public void createPost(Post newPost){
+        POSTS.add(newPost);
     }
 }
