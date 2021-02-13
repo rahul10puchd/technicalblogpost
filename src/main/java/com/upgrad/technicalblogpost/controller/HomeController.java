d14c20b710a94cc80fbea981cba84a2a62171419
Rahul Kumar <rahul10.pu@gmail.com>
2021-02-13 10:27:48 +0530

    13Feb2021 created API, getAllPost and getPostByUsername

--- src/main/java/com/upgrad/technicalblogpost/controller/HomeController.java
+++ src/main/java/com/upgrad/technicalblogpost/controller/HomeController.java
@@ -3,14 +3,19 @@ package com.upgrad.technicalblogpost.controller;
 import com.upgrad.technicalblogpost.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Date;
-@Controller
+//1. Controller -> RestController
+@RestController
 public class HomeController {
-    @RequestMapping("/")
-    public String getAllPosts(Model model){
+    @RequestMapping("/api/getposts")
+    //2.We don't require Model anymore...so delete it
+    public ArrayList<Post> getAllPosts(){//5. correct the return type of the function
 
         ArrayList<Post> posts = new ArrayList<>();
 
@@ -32,8 +37,19 @@ public class HomeController {
         posts.add(post1);
         posts.add(post2);
         posts.add(post3);
+        //3. delete model.addAttribute also
 
-        model.addAttribute("posts",posts);
-        return "index";
+        return posts;//4. returning posts
+    }
+//6. create a getpost by username API
+    @RequestMapping("/api/getposts/{user1}")
+    public Post getPostByUserName(@PathVariable(value = "user1") String user){
+        Post post1= new Post();
+//        if (user.equals("rahul")){
+            post1.setTitle("Mirzapur");
+            post1.setBody("Kalin Bhaia ki sarkar");
+            post1.setDate(new Date());
+
+        return post1;
     }
 }
