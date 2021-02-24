ce8d8cba1d412fac478b0b3f4b678d8f06a0be56
Rahul Kumar <rahul10.pu@gmail.com>
2021-02-24 10:48:05 +0530

    added entityManagerFactory method

---
+++ src/main/java/com/upgrad/technicalblogpost/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.upgrad.technicalblogpost.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+public class JpaConfig {
+    @Bean
+    public EntityManagerFactory entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean emfb= new LocalContainerEntityManagerFactoryBean();
+        emfb.setPersistenceXmlLocation("classpath:META-INF/persistence.xml");
+        emfb.afterPropertiesSet();
+        return  emfb.getObject();
+    }
+}
